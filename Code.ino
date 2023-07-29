cf2a8be621b34c3a786dea255edd539f9ab90d80
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-29 14:25:31 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,9 +1,18 @@
+
 void setup(){
+ const int BP = 3;
+ pinMode(BP, INPUT);
+ int test = digitalRead(BP);
  int coin = 1;
  while(coin==0){
- digitalWrite(LED_BUILTIN,1);
- delay(1000); 
- digitalWrite(LED_BUILTIN,0);
+  digitalWrite(LED_BUILTIN,1);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN,0);
+  test = digitalRead(BP);
+  if(test==LOW)
+  {
+   coin=1;
+  }
  }
  digitalWrite(LED_BUILTIN,0);
 }
