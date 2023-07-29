ac9ecbe29ddb8aff41a84dfaac60120c46556973
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-07-29 14:13:19 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,12 +1,12 @@
 void setup(){
  int coin = 1;
  while(coin==0){
- digitalWrite(LED_BUITIN,1)
+ digitalWrite(LED_BUILTIN,1);
  delay(1000); 
- digitalWrite(LED_BUITIN,0)
+ digitalWrite(LED_BUILTIN,0);
  }
- digitalWrite(LED_BUITIN,0)
+ digitalWrite(LED_BUILTIN,0);
 }
-void loop{
+void loop(){
 
 }
