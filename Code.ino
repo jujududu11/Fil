e2a7012779a3547e07f413758d4e09598d694482
jujududu11 <86871498+jujududu11@users.noreply.github.com>
2023-08-01 23:13:33 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -2,13 +2,12 @@
 void setup(){
  #define start 3
  const uint8_t ledPins[] = {4};
- int coin = 1;
+ int coin = 0;
  while(coin==0){
   digitalWrite(LED_BUILTIN,1);
   delay(1000); 
   digitalWrite(LED_BUILTIN,0);
-  test = start;
-  if(test==LOW)
+  if(start==LOW)
   {
    coin=1;
    ledPins[0] = 1
